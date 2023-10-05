e2cd99b4b1fd00edd6f56ec919745f1a19365c6e
Iankiburio <ian.kiburio@student.moringaschool.com>
2023-10-05 16:26:33 +0300

    complete

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
+    sequence = [0,1]
+
+    if length <=2:
+        print(sequence[:length])
+    else:
+        while len(sequence)< length:
+            next_number = sequence [-1] + sequence[-2]
+            sequence.append(next_number)
+
+        print(sequence)    
     pass
